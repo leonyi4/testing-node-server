6eea079b4c52b7628404397e3152612580f53c07
Leo <114857318+leonyi4@users.noreply.github.com>
2022-11-28 18:25:04 +0700

    added routing

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input name='message' type='text'><button type='submit'>Send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "Dummy");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
 
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
