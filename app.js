532b3bebb6cf59f3b482e3817ee34be6bf92c0ea
Leo <114857318+leonyi4@users.noreply.github.com>
2022-11-28 23:58:42 +0700

    finished the course section

--- app.js
+++ app.js
@@ -1,32 +1,7 @@
 const http = require("http");
-const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Message</title></head>");
-    res.write(
-      "<body><form action='/message' method='POST'><input name='message' type='text'><button type='submit'>Send</button></form></body>"
-    );
-    res.write("</html>");
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "Dummy");
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
-  }
-
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>First page</title></head>");
-  res.write("<body><h1>Hello, this is my node server!</h1></body>");
-  res.write("</html>");
-  res.end();
-});
+const routes = require("./routes");
+console.log(routes.someText);
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
