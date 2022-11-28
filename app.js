174453fdb96a4696112e7376c66f11d9439163f0
Leo <114857318+leonyi4@users.noreply.github.com>
2022-11-28 18:05:54 +0700

    set header

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>First page</title></head>");
+  res.write("<body><h1>Hello, this is my node server!</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
